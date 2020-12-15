f8cb5d488855ac5358e98d6e285ca5d353c86a5f
alenahalm <alenahalmatova749@gmail.com>
2020-12-15 21:46:55 +0800

    Initial commit

---
+++ apex/apex.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	cout << "Enter a, b, c" << endl;
+	cin >> a >> b >> c;
+	while (a == 0) {
+		cout << "Wrong input. Enter a" << endl;
+		cin >> a;
+	}
+	double y, x;
+	x = -b / (2 * a);
+	y = a * x * x + b * x + c;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	system("pause");
+}
